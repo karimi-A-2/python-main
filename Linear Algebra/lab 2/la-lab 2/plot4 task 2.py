5211f4bd486caae42a80b0c9d8c22b54085510fd
alikarimi <karimi.me.ali@gmail.com>
2022-10-13 18:50:10 +0330

    complete plot4 tasks

---
+++ Linear Algebra/lab 2/la-lab 2/plot4 task 2.py
@@ -0,0 +1,33 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+matplotlib.use('macosx')
+# %matplotlib
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+u = np.array([1, 2, 3])
+v = np.array([2, 0, -2])
+
+rng = np.linspace(-0.5, 1.5, 20)  # inclusive
+
+for alpha in rng:
+    w = (1 - alpha) * u + alpha * v
+    
+    ax.set_xlim(-4, 4)
+    ax.set_ylim(-4, 4)
+    ax.set_zlim(-4, 4)
+    
+    ax.quiver(0, 0, 0, u[0], u[1], u[2], color='r')
+    ax.quiver(0, 0, 0, v[0], v[1], v[2], color='r')
+    
+    ax.quiver(0, 0, 0, w[0], w[1], w[2], color='b')
+    ax.scatter(w[0], w[1], w[2], color='b')
+
+    plt.draw()
+    plt.pause(.1)
+    
+plt.show()
