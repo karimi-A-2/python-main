99e429f034afdf6a9d3f639b43e4e635425b9e39
alikarimi <karimi.me.ali@gmail.com>
2022-10-31 18:58:36 +0330

    do task 1

--- Linear Algebra/lab 5/task1.py
+++ Linear Algebra/lab 5/task1.py
@@ -1,20 +1,23 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+matplotlib.use('macosx')
 
 from face_data import Face1, Face2, Face3, TargetFace2, edges
 
 
 def plot_face(plt, X, edges, color='b'):
-    "plots a face"
-
+    """plots a face"""
+    
     plt.plot(X[:, 0], X[:, 1], 'o', color=color, markersize=3)
-
+    
     for i, j in edges:
         xi = X[i, 0]
         yi = X[i, 1]
         xj = X[j, 0]
         yj = X[j, 1]
-
+        
         # draw a line between X[i] and X[j]
         plt.plot((xi, xj), (yi, yj), '-', color=color)
     plt.axis('square')
@@ -29,8 +32,10 @@ t = TargetFace2.ravel()
 
 F = np.stack((face1, face2, face3), axis=1)
 
-x = # solve the equation
-
+faces = F[0:3, :]
+target = t[0:3].target((3, 1))
+x = np.linalg.inv(faces) @ target
+x = x.round(decimals=1).target((3,)).tolist()
 a, b, c = x
 
 Face = a * Face1 + b * Face2 + c * Face3
--- Linear Algebra/lab 5/task2.py
+++ Linear Algebra/lab 5/task2.py
@@ -1,20 +1,23 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+matplotlib.use('macosx')
 
 from face_data import Face1, Face2, Face3, TargetFace2, edges
 
 
 def plot_face(plt, X, edges, color='b'):
-    "plots a face"
-
+    """plots a face"""
+    
     plt.plot(X[:, 0], X[:, 1], 'o', color=color, markersize=3)
-
+    
     for i, j in edges:
         xi = X[i, 0]
         yi = X[i, 1]
         xj = X[j, 0]
         yj = X[j, 1]
-
+        
         # draw a line between X[i] and X[j]
         plt.plot((xi, xj), (yi, yj), '-', color=color)
     plt.axis('square')
@@ -32,10 +35,9 @@ F = np.stack((face1, face2, face3), axis=1)
 for i in range(5):
     inds = np.random.choice(range(136), 3, replace=False)
     # choose the equations
-    a,b,c = # solve the equations
-
+    a, b, c =  # solve the equations
+    
     Face = a * Face1 + b * Face2 + c * Face3
     plot_face(plt, TargetFace2, edges, color='r')
     plot_face(plt, Face, edges, color='g')
     plt.show()
-
