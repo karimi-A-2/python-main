c8c161838a29b8e407faa957d7e58062f8b23e3d
alikarimi <karimi.me.ali@gmail.com>
2022-12-15 20:39:39 +0330

    add random and least square solutions

--- Linear Algebra/lab 6/task1.py
+++ Linear Algebra/lab 6/task1.py
@@ -1,26 +1,29 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+matplotlib.use('macosx')
 
 from face_data import Face1, Face2, Face3, TargetFace2, edges
 
 
-def plot_face(plt,X,edges,color='b'):
+def plot_face(plt, X, edges, color='b'):
     "plots a face"
     
-    plt.plot(X[:,0], X[:,1], 'o', color=color, markersize=3)
-
-    for i,j in edges:
-        xi = X[i,0]
-        yi = X[i,1]
-        xj = X[j,0]
-        yj = X[j,1]
-
+    plt.plot(X[:, 0], X[:, 1], 'o', color=color, markersize=3)
+    
+    for i, j in edges:
+        xi = X[i, 0]
+        yi = X[i, 1]
+        xj = X[j, 0]
+        yj = X[j, 1]
+        
         # draw a line between X[i] and X[j]
-        plt.plot((xi,xj), (yi,yj), '-', color=color)
-
+        plt.plot((xi, xj), (yi, yj), '-', color=color)
+    
     plt.axis('square')
-    plt.xlim(-100,100)
-    plt.ylim(-100,100)
+    plt.xlim(-100, 100)
+    plt.ylim(-100, 100)
 
 
 TargetFace = TargetFace2
@@ -35,9 +38,13 @@ F = np.stack((face1, face2, face3), axis=1)
 
 for i in range(5):
     inds = np.random.choice(range(136), 3, replace=False)
+    faces = F[inds, :]
+    target = t[inds].reshape((3, 1))
+    x1 = np.linalg.solve(faces, target)
+    a1, b1, c1 = x1.ravel().tolist()
     
-    a1,b1,c1 = # solve 3 random equations
-    a2,b2,c2 = # least squares solution
+    x2 = np.linalg.inv(F.T @ F) @ F.T @ t
+    a2, b2, c2 = x2.ravel().tolist()
 
     Face_rnd = a1 * Face1 + b1 * Face2 + c1 * Face3
     Face_lsq = a2 * Face1 + b2 * Face2 + c2 * Face3
