e5ff8ad04d59993fda3bddb56a15992c793f2421
alikarimi <karimi.me.ali@gmail.com>
2022-12-16 10:21:03 +0330

    add lab bonus 1 files

---
+++ Linear Algebra/lab bonus 1/task1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def solve_equatoins(a:np.array, b:np.array) -> np.array:
+    # return np.linalg.solve(a, b)
+    pass
+
+
+def main():
+    n = int(input())
+    a = []
+    for _ in range(n):
+        a.append(list(map(float, input().split())))
+
+    a = np.array(a)
+
+    b = np.array(list(map(float, input().split())))
+
+    print(solve_equatoins(a, b))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Linear Algebra/lab bonus 1/task2.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def inverse(a:np.array) -> np.array:
+    # return np.linalg.inv(a)
+    pass
+
+
+def main():
+    n = int(input())
+    a = []
+    for _ in range(n):
+        a.append(list(float(int, input().split())))
+
+    a = np.array(a)
+
+    print(inverse(a))
+
+
+if __name__ == "__main__":
+    main()
